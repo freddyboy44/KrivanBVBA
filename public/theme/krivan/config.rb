5d85926caef6fe89f09c6f1e2d7a057abed83567
Frederik Boi <boifrederik@telenet.be>
2015-06-16 17:51:59 +0200

    Eerste werkdag

---
+++ public/theme/krivan/config.rb
@@ -0,0 +1,9 @@
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = true
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
